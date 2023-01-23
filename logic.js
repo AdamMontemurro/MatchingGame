95d6d1af5511602926bf858c2757353c85680fc5
adam <adammontemurro@gmail.com>
2023-01-23 13:11:04 -0500

    changed size and display to contain image so it doesn't stretch to new size

--- logic.js
+++ logic.js
@@ -1,124 +1,123 @@
 function main() {
-let attempts = 0
-let matches = 0
-let picked = []
-let pickedID= []
-const cards = [
-  {
-    name: 'fox',
-    picture: 'pictures/fox.png'
+  let attempts = 0
+  let matches = 0
+  let picked = []
+  let pickedID = []
+  const cards = [
+    {
+      name: 'fox',
+      picture: 'pictures/fox.png'
 
-  },
-  {
-    name: 'falco',
-    picture: 'pictures/falco.png'
+    },
+    {
+      name: 'falco',
+      picture: 'pictures/falco.png'
 
-  },
-  {
-    name: 'peach',
-    picture: 'pictures/Peach.png'
+    },
+    {
+      name: 'peach',
+      picture: 'pictures/Peach.png'
 
-  },
-  {
-    name: 'mario',
-    picture: 'pictures/Mario.png'
+    },
+    {
+      name: 'mario',
+      picture: 'pictures/Mario.png'
 
-  },
-  {
-    name: 'falcon',
-    picture: 'pictures/Falcon.png'
+    },
+    {
+      name: 'falcon',
+      picture: 'pictures/Falcon.png'
 
-  },
-  {
-    name: 'marth',
-    picture: 'pictures/Marth.png'
+    },
+    {
+      name: 'marth',
+      picture: 'pictures/Marth.png'
 
-  },
-  {
-    name: 'fox',
-    picture: 'pictures/fox.png'
+    },
+    {
+      name: 'fox',
+      picture: 'pictures/fox.png'
 
-  },
-  {
-    name: 'falco',
-    picture: 'pictures/falco.png'
+    },
+    {
+      name: 'falco',
+      picture: 'pictures/falco.png'
 
-  },
-  {
-    name: 'peach',
-    picture: 'pictures/Peach.png'
+    },
+    {
+      name: 'peach',
+      picture: 'pictures/Peach.png'
 
-  },
-  {
-    name: 'mario',
-    picture: 'pictures/Mario.png'
+    },
+    {
+      name: 'mario',
+      picture: 'pictures/Mario.png'
 
-  },
-  {
-    name: 'falcon',
-    picture: 'pictures/Falcon.png'
+    },
+    {
+      name: 'falcon',
+      picture: 'pictures/Falcon.png'
 
-  },
-  {
-    name: 'marth',
-    picture: 'pictures/Marth.png'
+    },
+    {
+      name: 'marth',
+      picture: 'pictures/Marth.png'
 
+    }
+  ]
+  cards.sort(() => Math.random() - 0.5)
+
+
+  const boxes = Array.from(document.getElementsByClassName("box"))
+
+
+  for (box of boxes) {
+    box.addEventListener('click', boxClicked)
   }
-]
-cards.sort( ()=> Math.random() - 0.5 )
 
 
-const boxes = Array.from(document.getElementsByClassName("box"))
+  function checkWin() {
 
-
-for (box of boxes){
-  box.addEventListener('click', boxClicked)
-}
-
-
-function checkWin() {
-  
-  if (picked[0]==picked[1]){
-    boxes[pickedID[0]].classList.add('picked')
-    boxes[pickedID[1]].classList.add('picked')
-    boxes[pickedID[0]].removeEventListener('click',boxClicked)
-    boxes[pickedID[1]].removeEventListener('click',boxClicked)
-    picked=[]
-    pickedID=[]
-    matches+=1
-    document.querySelector('#matches').innerHTML= matches
-      if (matches===6){
+    if (picked[0] == picked[1]) {
+      boxes[pickedID[0]].classList.add('picked')
+      boxes[pickedID[1]].classList.add('picked')
+      boxes[pickedID[0]].removeEventListener('click', boxClicked)
+      boxes[pickedID[1]].removeEventListener('click', boxClicked)
+      picked = []
+      pickedID = []
+      matches += 1
+      document.querySelector('#matches').innerHTML = matches
+      if (matches === 6) {
         const win = new Audio("sounds/complete.wav")
         win.play()
-      }else {
+      } else {
         return
       }
-}
-    else if (picked[0]!==picked[1]){
-      boxes[pickedID[0]].addEventListener('click',boxClicked)
-      boxes[pickedID[1]].addEventListener('click',boxClicked)
+    }
+    else if (picked[0] !== picked[1]) {
+      boxes[pickedID[0]].addEventListener('click', boxClicked)
+      boxes[pickedID[1]].addEventListener('click', boxClicked)
       boxes[pickedID[0]].setAttribute("src", "pictures/shine.png")
       boxes[pickedID[1]].setAttribute("src", "pictures/shine.png")
-      picked=[]
-      pickedID=[]
+      picked = []
+      pickedID = []
     }
-}
-
-
-function boxClicked() {
-  
-  const id = this.getAttribute('data-id')
-  this.setAttribute("src",cards[id].picture)
-  pickedID.push(id)
-  picked.push(cards[id].name)
-  this.removeEventListener('click',boxClicked)
-  if (picked.length == 2){
-    setTimeout(checkWin, 400)
-    attempts +=1
-    document.querySelector('#attempt').innerHTML= attempts
-
   }
-}
+
+
+  function boxClicked() {
+
+    const id = this.getAttribute('data-id')
+    this.setAttribute("src", cards[id].picture)
+    pickedID.push(id)
+    picked.push(cards[id].name)
+    this.removeEventListener('click', boxClicked)
+    if (picked.length == 2) {
+      setTimeout(checkWin, 400)
+      attempts += 1
+      document.querySelector('#attempt').innerHTML = attempts
+    }
+  }
 
 
 }
